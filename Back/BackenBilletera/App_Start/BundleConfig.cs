f4a449843db15c053c5178fce316ba70c2343d37
Lisandro Michlig <lichimichlig@gmail.com>
2020-12-26 21:28:27 -0300

    Correccion Bundles

--- Back/BackenBilletera/App_Start/BundleConfig.cs
+++ Back/BackenBilletera/App_Start/BundleConfig.cs
@@ -8,29 +8,33 @@ namespace BackenBilletera
         // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            bundles.Add(new Bundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            bundles.Add(new Bundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
             // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
             // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            bundles.Add(new Bundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
+            bundles.Add(new Bundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
-            bundles.Add(new ScriptBundle("~/Script/Bundles").Include(
-                      "~/bundles/runtime.*",
-                      "~/bundles/zone.*",
-                      "~/bundles/ployies.*",
-                      "~/bundles/main.*"));
-            bundles.Add(new StyleBundle("~/Content/Styles").Include("~/bundles/styles.*"));
+            bundles.Add(new Bundle("~/Script/bundles").Include(
+                    "~/bundles/main.*",
+                    "~/bundles/inline.*",
+                    "~/bundles/polyfills.*",
+                    "~/bundles/scripts.*",
+                    "~/bundles/vendor.*",
+                    "~/bundles/runtime.*",
+                    "~/bundles/zone.*"
+                    ));
+            bundles.Add(new Bundle("~/Content/Styles").Include("~/bundles/styles.*"));
         }
     }
 }
