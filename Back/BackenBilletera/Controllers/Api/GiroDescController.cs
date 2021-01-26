3b3b8ba442715a7f06e1a079d12c86b38cfbc5f9
Lisandro Michlig <lichimichlig@gmail.com>
2021-01-26 20:01:50 -0300

    Assets y model de giro descubierto (Aun sin hacer)

---
+++ Back/BackenBilletera/Controllers/Api/GiroDescController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.Description;
+using BackenBilletera.Models;
+
+namespace BackenBilletera.Controllers.Api
+{
+    [Authorize]
+    public class GiroDescController : ApiController
+    {
+        private DBbilleteraEntities db = new DBbilleteraEntities();
+
+       
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        
+        public IHttpActionResult GiroDesc(GiroDesc modelo)
+        {
+            
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+       
+    }
+}
---
+++ Back/BackenBilletera/Models/GiroDesc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackenBilletera.Models
+{
+    public class GiroDesc
+    {
+        public int idUsuario { get; set; }
+        public string noCubierto { get; set; }
+        public int monto { get; set; }
+    }
+}
